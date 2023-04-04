ab88f3483ddadd312330ae66792a22a08bc93242
usaama256 <usaamankangi@gmail.com>
2023-04-04 14:32:22 +0300

    opil

---
+++ house_work_mgt/src/layouts/dashboard/layout.js
@@ -0,0 +1,55 @@
+import { useCallback, useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import { styled } from "@mui/material/styles";
+import { withAuthGuard } from "src/hocs/with-auth-guard";
+import { SideNav } from "./side-nav";
+import { TopNav } from "./top-nav";
+import { Box } from "@mui/material";
+
+const SIDE_NAV_WIDTH = 280;
+
+const LayoutRoot = styled("div")(({ theme }) => ({
+  display: "flex",
+  flex: "1 1 auto",
+  maxWidth: "100%",
+  [theme.breakpoints.up("lg")]: {
+    paddingLeft: SIDE_NAV_WIDTH,
+  },
+}));
+
+const LayoutContainer = styled("div")(({ theme }) => ({
+  display: "flex",
+  flex: "1 1 auto",
+  flexDirection: "column",
+  width: "100%",
+}));
+
+export const Layout = withAuthGuard((props) => {
+  const { children } = props;
+  const pathname = usePathname();
+  const [openNav, setOpenNav] = useState(false);
+
+  const handlePathnameChange = useCallback(() => {
+    if (openNav) {
+      setOpenNav(false);
+    }
+  }, [openNav]);
+
+  useEffect(
+    () => {
+      handlePathnameChange();
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [pathname]
+  );
+
+  return (
+    <Box sx={{ backgroundColor: "neutral.900" }}>
+      <TopNav onNavOpen={() => setOpenNav(true)} />
+      <SideNav onClose={() => setOpenNav(false)} open={openNav} />
+      <LayoutRoot sx={{ backgroundColor: "neutral.900" }}>
+        <LayoutContainer>{children}</LayoutContainer>
+      </LayoutRoot>
+    </Box>
+  );
+});
