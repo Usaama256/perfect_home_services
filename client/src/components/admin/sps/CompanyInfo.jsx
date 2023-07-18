619bf4557a14dbd39e7038ef98cbc3c52f47f23b
usaama256 <usaamankangi@gmail.com>
2023-07-18 10:33:41 +0300

    sd

--- client/src/components/admin/sps/CompanyInfo.jsx
+++ client/src/components/admin/sps/CompanyInfo.jsx
@@ -192,7 +192,7 @@ const CompanyInfo = ({ data, setCurrentSP }) => {
       )}
       <CardContent sx={{ pt: 0 }}>
         <Box sx={{ margin: "20px 0px 10px 0px" }}>
-          <Grid container spacing={3} columns={12}>
+          <Stack flexDirection="row" justifyContent="center" gap={2}>
             <Grid container spacing={1} xs={12} md={6} columns={6}>
               <Grid item xs={12} md={6}>
                 <Stack
@@ -316,7 +316,7 @@ const CompanyInfo = ({ data, setCurrentSP }) => {
                 />
               </Grid>
             </Grid>
-          </Grid>
+          </Stack>
         </Box>
       </CardContent>
       <Divider />
--- client/src/components/admin/sps/OwnerInfo.jsx
+++ client/src/components/admin/sps/OwnerInfo.jsx
@@ -19,7 +19,7 @@ const OwnerInfo = ({ data }) => {
       />
       <CardContent sx={{ pt: 0 }}>
         <Box sx={{ margin: "20px 0px 10px 0px" }}>
-          <Grid container spacing={3} columns={12}>
+          <Stack flexDirection="row" justifyContent="center" gap={2}>
             <Grid container spacing={1} xs={12} md={6} columns={6}>
               <Grid item xs={12} md={6}>
                 <Stack
@@ -111,7 +111,7 @@ const OwnerInfo = ({ data }) => {
                 />
               </Grid>
             </Grid>
-          </Grid>
+          </Stack>
         </Box>
       </CardContent>
       <Divider />
